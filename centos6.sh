3dbd8b8df81f27847bbf7c026768b8c5264b44aa
sktaka <yussz.ay84@gmail.com>
2016-07-22 02:21:42 +0800

    Update centos6.sh

--- centos6.sh
+++ centos6.sh
@@ -158,9 +158,9 @@ cp client.tar /home/vps/public_html/
 cd
 
 # install badvpn
-wget -O /usr/bin/badvpn-udpgw "http://script.jualssh.com/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/sktaka/webmincentos6/master/conf/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "http://script.jualssh.com/badvpn-udpgw64"
+  wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/sktaka/webmincentos6/master/conf/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.d/rc.local
