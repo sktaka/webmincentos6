6172fddecfa5c8c08b321d12176b1063986a346e
sktaka <yussz.ay84@gmail.com>
2016-07-22 02:44:16 +0800

    Update centos6.sh

--- centos6.sh
+++ centos6.sh
@@ -35,7 +35,7 @@ rpm -Uvh epel-release-6-8.noarch.rpm
 rpm -Uvh remi-release-6.rpm
 
 if [ "$OS" == "x86_64" ]; then
-  wget https://raw.github.com/sktaka/webmincentos6/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+  wget https://raw.github.com/sktaka/webmincentos6/master/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
   rpm -Uvh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 else
   wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.i686.rpm
