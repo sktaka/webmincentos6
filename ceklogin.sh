2d4f953477d6437e27b4c237c94083f927f44761
sktaka <yussz.ay84@gmail.com>
2016-07-24 03:24:22 +0800

    Add files via upload

---
+++ ceklogin.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright by bangor ssh premium
+# ========================
+# 
+
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+
+echo "Pengecekan Login dropbear";
+echo "---";
+
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+	if [ $NUM -eq 1 ]; then
+		echo "$PID - $USER - $IP";
+	fi
+done
+echo "---";
+
+data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+
+echo "Pengecekan Login OpenSSH";
+echo "---";
+for PID in "${data[@]}"
+do
+        #echo "check $PID";
+        NUM=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+        USER=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+        IP=`cat /var/log/secure | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+done
+
+# yg ini jgn di edit ya
+echo "---";
+echo "TakaVPN";
